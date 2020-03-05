c8248830cafaf832774d510922f2f42293ce5ec2
rajat <rraikarnvi@nvi.co.in>
2020-03-05 17:55:55 +0530

    state prop mapping

--- packages/common/src/App.tsx
+++ packages/common/src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import CounterApp from './screens/CounterApp';
 import { Provider } from "react-redux";
-import {store} from './store'
+import { store } from './store'
+import CounterApp from './screens/CounterApp';
 
 export default function App() {
   return (
     <Provider store={store}>
-    <CounterApp></CounterApp>
+      <CounterApp></CounterApp>
     </Provider>
   );
 };
--- packages/common/src/screens/CounterApp.tsx
+++ packages/common/src/screens/CounterApp.tsx
@@ -1,31 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, Dispatch } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { Observable, Subscription } from 'rxjs';
 import { store } from '../store';
 import { DECREMENT_COUNTER, INCREMENT_COUNTER } from '../store/constants';
 import { RootState } from '../store/reducers';
+import { CounterState } from '../store/reducers/counterReducer';
+import { ActionsType, RootStateType } from '../store/epics';
+import { connect } from 'react-redux';
 
-const CounterApp = () => {
+const mapStateToProps = (state: RootStateType) => {
+    return {
+        countState: state.counter,
+    }
+}
 
-    const state$ = getState$(store);
-    const [state, counter] = useState(store.getState().counter)
-    let subscription: Subscription;
-
-    useEffect(() => {
-        console.log('componentDidMount!');
-        subscription = state$.subscribe(function (state: RootState) {
-            counter(state.counter);
-        });
-    }, []);
-
-    useEffect(() => {
-        return () => {
-            console.log('will unmount');
-            subscription.unsubscribe();
+const mapDispatchToProps = (dispatch: Dispatch<ActionsType>) => {
+    return {
+        onIncreaseClick: () => {
+            dispatch({ type: INCREMENT_COUNTER });
         }
-    }, []);
+    }
+};
 
-    useEffect(() => console.log('mounted or updated'));
+export interface CounterProps {
+    countState: CounterState;
+  }
+
+// const CounterApp: React.SFC<CounterState> = (counter :CounterState) => {
+    export const CounterApp = ({ countState }:CounterProps) => {
+
+    // const state$ = getState$(store);
+    // const [state, counter] = useState(store.getState().counter)
+    // let subscription: Subscription;
+
+    // useEffect(() => {
+    //     console.log('componentDidMount!');
+    //     subscription = state$.subscribe(function (state: RootState) {
+    //         counter(state.counter);
+    //     });
+    // }, []);
+
+    // useEffect(() => {
+    //     return () => {
+    //         console.log('will unmount');
+    //         subscription.unsubscribe();
+    //     }
+    // }, []);
+
+    // useEffect(() => console.log('mounted or updated'));
+
+    console.log("state");
+    
+    console.log(countState.counter);
 
     return (
         <View style={styles.container}>
@@ -33,7 +59,7 @@ const CounterApp = () => {
                 <Button title="increment" onPress={() => {
                     store.dispatch({ type: INCREMENT_COUNTER });
                 }}></Button>
-                {state.counter > 2 ? <Text>Name: {state.counter}</Text> : <Text>Cool: {state.counter}</Text>}
+                {countState.counter > 2 ? <Text>Name: {countState.counter}</Text> : <Text>Cool: {countState.counter}</Text>}
                 <Text>{}</Text>
                 <Button title="decrement" onPress={() => {
                     store.dispatch({ type: DECREMENT_COUNTER, payload: { count: 0 } });
@@ -57,7 +83,7 @@ const getState$ = (store: any) => {
     });
 }
 
-export default CounterApp;
+export default connect(mapStateToProps, mapDispatchToProps)(CounterApp);
 
 const styles = StyleSheet.create({
     container: {
